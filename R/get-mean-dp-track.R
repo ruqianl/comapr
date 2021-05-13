975ed6310bc3316333a37c98dc37625292977ebf
Ubuntu <ubuntu@ruqian-lyu.novalocal>
2021-05-13 23:30:46 +0000

    update README

--- R/get-mean-dp-track.R
+++ R/get-mean-dp-track.R
@@ -63,3 +63,89 @@ getMeanDPTrack <- function( chrom = "chr1",
                              background.title = "lightblue")
   meanDP_track
 }
+
+
+
+#' getCellDPTrack
+#' Generates the DataTrack for plotting DP of a selected cell
+#'
+#' It plots the total allele counts for the selected cell.
+#' @inheritParams getCellAFTrack
+#' @param chrom, the chromosome
+#' @param path_loc, the path prefix to the output files from sscocaller including
+#' "*_totalCount.mtx"
+#'
+#' @param nwindow, the number of windows for binning the chromosome
+#' @param barcodeFile, the barcode file containing the list of cell barcodes used
+#' as the input file for sscocaller
+#' @param cellBarcode, the selected cell barcode
+#' @param snp_track, the SNP position track which is used for obtaining the SNP
+#' chromosome locations. It could be omitted and the SNP positions will be acquired
+#' from the "*_snpAnnot.txt" file.
+
+#' @return The DataTrack object defined in \code{\link[Gviz]{DataTrack}}
+#' @importFrom  Gviz DataTrack
+#' @author Ruqian Lyu
+#' @export
+#' @examples
+#' demo_path <-paste0(system.file("extdata",package = "comapr"),"/")
+#' s1_rse_state <- readHapState("s1",chroms=c("chr1"),
+#'                              path=demo_path,barcodeFile=NULL,minSNP = 0,
+#'                              minlogllRatio = 50,
+#'                              bpDist = 100,maxRawCO=10,
+#'                              minCellSNP = 0)
+#' s1_counts <- countCOs(s1_rse_state)
+#' dp_co_tracks <- getCellDPTrack(chrom ="chr1",
+#'                                path_loc = demo_path,
+#'                                sampleName = "s1",
+#'                                barcodeFile = paste0(demo_path,
+#'                                                     "s1_barcodes.txt"),
+#'                                cellBarcode = "BC1")
+#'
+getCellDPTrack <-  function(chrom = "chr1",
+                            path_loc = "./output/firstBatch/WC_522/",
+                            sampleName = "WC_522",
+                            nwindow = 80,
+                            barcodeFile,
+                            cellBarcode,
+                            snp_track = NULL,
+                            chunk = 1000L,
+                            log =TRUE,
+                            plot_type = "hist"){
+  stopifnot(length(cellBarcode)==1)
+  initial_barcodes <- read.table(file = barcodeFile)
+  whichCell <- match(cellBarcode, initial_barcodes$V1)
+
+  dpMM <- readColMM(file = paste0(path_loc, sampleName,"_",
+                                  chrom,"_totalCount.mtx"),
+                    which.col = whichCell,
+                    chunk = chunk)
+
+  dpMM <-dpMM[,whichCell]
+
+  if(is.null(snp_track)){
+    snp_anno <- read.table(file=paste0(path_loc,sampleName,"_",
+                                       chrom, "_snpAnnot.txt"),
+                           header=TRUE)
+    snp_pos <- snp_anno$POS
+
+  } else {
+    stopifnot(unlist(dimnames(seqinfo(snp_track))) == chrom)
+    snp_pos <- snp_track@range@ranges@start
+  }
+  aggregation_fun <- ifelse(log, function(x) { log10(mean(x)+1)}, "mean")
+
+  dp_track <- Gviz::DataTrack(GRanges(seqnames = chrom,
+                                      IRanges(start=snp_pos,
+                                              width = 1,
+                                              genome = "mm10")),
+                              name = paste0(cellBarcode, " AF"),
+                              data = dpMM,
+                              window = nwindow,
+                              na.rm=TRUE,
+                              aggregation = aggregation_fun,
+                              type=plot_type)
+
+  dp_track
+}
+
